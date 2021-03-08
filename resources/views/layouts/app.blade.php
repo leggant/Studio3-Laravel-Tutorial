0ad022bb47c196626de816c9cc530ac978a08eeb
Anthony Legg <leggtc1@student.op.ac.nz>
2021-03-08 16:20:06 +1300

    To Do App-Stage 2
    completed first todos table migration, created the todo model and Todos Controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}"> {{-- <- bootstrap css --}}
-    <title>@yield('title','Laravel 5.8 Basics')</title>
+    <title>@yield('title','Laravel To Do App')</title>
 </head>
 <body>
     {{-- That's how you write a comment in blade --}}
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -18,7 +18,22 @@
             </ul>
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
+            <div class="flex-center position-ref">
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
             </ul>
         </div>
+        
     </div>
 </nav>
