62529603865dd1cb54216d284c244341d59d4a41
Anthony Legg <leggtc1@student.op.ac.nz>
2021-03-11 18:46:25 +1300

    2nd Attempt To-Do App
    using a youtube tutorial found at: https://www.youtube.com/watch?v=wDCY277XBh8

---
+++ to-do-app-2/resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>My To Dos App - Laravel</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
